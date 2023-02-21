84f196d7608c3a0c11386715470416ac3eddd2c9
مهرداد تابش <mehrdadit12@gmail.com>
2023-02-21 04:26:20 -0800

    پیاده سازی چندی از عملیات حساب بانکی به کمک الگوی facade

---
+++ ServiceBacnkProject/BankService.cs
@@ -0,0 +1,43 @@
+﻿namespace ServiceBacnkProject;
+
+//Facade
+public class BankService
+{
+    private Dictionary<int, IAccount> bankAccounts;
+
+    public BankService()
+    {
+        bankAccounts = new Dictionary<int, IAccount>();
+    }
+
+    public int CreateNewAccount(string type, decimal initAmount)
+    {
+        IAccount newAccount = null;
+        switch (type)
+        {
+            case "chequing":
+                newAccount = new Chequing(initAmount);
+                break;
+            case "saving":
+                newAccount = new Saving(initAmount);
+                break;
+            case "investment":
+                newAccount = new Investment(initAmount);
+                break;
+        }
+
+        if (newAccount != null)
+        {
+            bankAccounts.Add(newAccount.GetAccountNumber(), newAccount);
+            return newAccount.GetAccountNumber();
+        }
+        return -1;
+    }
+
+    public void TransferMoney(int to, int from, decimal amount)
+    {
+        IAccount toAccount = bankAccounts[to];
+        IAccount fromAccount = bankAccounts[from];
+        fromAccount.transfer(toAccount, amount);
+    }
+}
---
+++ ServiceBacnkProject/IAccount.cs
@@ -0,0 +1,100 @@
+﻿namespace ServiceBacnkProject;
+//Sub system classes
+public interface IAccount
+{
+    public decimal Amount { get; set; }
+
+    public void Deposit(decimal amount);
+    public void WithDraw(decimal amount);
+    public void transfer(IAccount account,decimal amount);
+    public int GetAccountNumber();
+}
+
+public class Chequing : IAccount
+{
+    public Chequing(decimal amount)
+    {
+        Amount = amount;
+    }
+
+    public decimal Amount { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    public void Deposit(decimal amount)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int GetAccountNumber()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void transfer(IAccount account, decimal amount)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void WithDraw(decimal amount)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class Saving : IAccount
+{
+    public Saving(decimal amount)
+    {
+        Amount = amount;
+    }
+
+    public decimal Amount { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    public void Deposit(decimal amount)
+    {
+        throw new NotImplementedException();
+    }
+    public int GetAccountNumber()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void transfer(IAccount account, decimal amount)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void WithDraw(decimal amount)
+    {
+        throw new NotImplementedException();
+    }
+}
+public class Investment : IAccount
+{
+    public Investment(decimal amount)
+    {
+        Amount = amount;
+    }
+
+    public decimal Amount { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    public void Deposit(decimal amount)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int GetAccountNumber()
+    {
+        throw new NotImplementedException();
+    }
+
+
+    public void transfer(IAccount account, decimal amount)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void WithDraw(decimal amount)
+    {
+        throw new NotImplementedException();
+    }
+}

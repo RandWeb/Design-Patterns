ba5e8435d5df5a5fa47d2423f7610793c22fd0e6
مهرداد تابش <mehrdadit12@gmail.com>
2023-12-28 09:47:44 -0800

    continue

---
+++ ComputerStore/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ComputerStore;
+
+Assembler assembler = new(new IntelComputer());
+var computer =  assembler.Assembel();
+Console.WriteLine(computer.ToString());
---
+++ CustomHttpClientRequestBuilder/Program.cs
@@ -0,0 +1,13 @@
+﻿using CustomeHttpClientRequestBuilder;
+using CustomeHttpClientRequestBuilder.Models;
+using CustomHttpClientRequestBuilder;
+
+IHttpRequestFactory requestFactory = new HttpRequestFactory();
+
+var response = await requestFactory
+    .PutAsync()
+    .AddUri("uri")
+    .AddBody("data")
+    .AddToken(new Token("token", TypeOfToken.Bearer))
+    .BuildAsync<object>();
+Console.WriteLine();

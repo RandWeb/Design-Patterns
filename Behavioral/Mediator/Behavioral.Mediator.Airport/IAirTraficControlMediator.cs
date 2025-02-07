ac3d08154b4208208382abc3f5a67be2666e546b
مهرداد تابش <mehrdadit12@gmail.com>
2025-02-07 04:38:47 -0800

    folders refactored

---
+++ Behavioral/Mediator/Behavioral.Mediator.Airport/IAirTraficControlMediator.cs
@@ -0,0 +1,9 @@
+﻿
+// mediator
+internal interface IAirTraficControlMediator
+{
+    void RegisterAirline(params AirlineBase[] airline);
+    void SendMessage(AirlineBase airlineBase, string message);
+}
+//AirbusAirLine,BoeingAirlineComponent
+
